7e4ae8a0e34a83576c8de7db90c8e555f28433c0
dhanushka365 <technohub10000@gmail.com>
2022-06-20 18:27:15 +0530

    finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChartsComponent } from './routes/charts/charts.component';
+import { LoginComponent } from './routes/login/login.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { ToggleComponent } from './toggle/toggle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:NavbarComponent},
+  {path:'static',component:ToggleComponent,pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'charts',component:ChartsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
